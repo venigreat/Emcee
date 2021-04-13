410fcbae55c8f426fc3e293bad3b39b92eeb395d
Andrey Belyaev <a.belyaev3@tinkoff.ru>
2021-04-13 16:41:58 +0300

    Заглушка для добавления авторизации к сервису

--- Sources/URLResource/URLResource.swift
+++ Sources/URLResource/URLResource.swift
@@ -77,7 +77,13 @@ public final class URLResourceImpl: URLResource {
     }
     
     private func startLoadingUrlResource(url: URL) {
-        let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 20)
+        var request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 20)
+        if url.host == "" {
+            Logger.debug("Add basic auth to \(url)")
+            request.setValue("Basic ", forHTTPHeaderField:"Authorization")
+        } else {
+            Logger.debug("Not add basic auth to \(url)")
+        }
         Logger.debug("Will fetch resource '\(url)'")
         let task = createDownloadTask(request: request, url: url)
         task.resume()
